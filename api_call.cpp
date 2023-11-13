40e2d23c5c8352172f09bf082a0ef4a56ef7d56b
kahdichinja <agooclinton@gmail.com>
2023-11-13 09:01:49 +0300

    network calls

---
+++ api_call.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include "httplib.h"
+#define CPPHTTPLIB_OPENSSL_SUPPORT 
+#include "json.hpp"
+using namespace std;
+using json = nlohmann::json;
+
+struct UserModel
+{
+    int id;
+    string name;
+    string username;
+    string email;
+
+
+    static UserModel fromJson(const json& userdata){
+
+        UserModel user;
+
+        user.id = userdata["id"];
+        user.name = userdata["name"];
+        user.username = userdata["username"];
+        user.email = userdata["email"];
+
+        return user;
+    }
+
+    json toJson() const {
+        return {
+            {"id", id},
+            {"name", name},
+            {"username", username},
+            {"email", email},
+        };
+    }
+};
+
+struct ErrorModel{
+    int code;
+    string message;
+};
+
+std::pair<UserModel, ErrorModel> getUserData() {
+
+    httplib::Client client("https://jsonplaceholder.typicode.com");
+
+    auto response = client.Get("/users");
+
+    if (response && response->status == 200)    
+    {
+        try
+        {
+            json jsondata = json::parse(response->body);
+            if (!jsondata.empty())
+            {
+                UserModel user = UserModel::fromJson(jsondata);
+
+                return {user, ErrorModel()};
+            }else  return {UserModel(), {404, "No user data found"}};
+            
+        }
+        catch(const json::parse_error& e)
+        {
+            return {UserModel(), {500, "Error parsing JSON: "+ string(e.what())}};
+        }
+        
+    }else
+    {
+        return {UserModel(), {response ? response->status : -1, "Network error"}};
+    }
+
+}
+
+
+int main()
+{
+    auto [user, error] = getUserData();
+
+    if (error.code != 0)
+    {
+        std::cerr << "Error code: " << error.code << ", Message: " << error.message << std::endl;
+    }else
+    {
+        // Use user data
+        std::cout << "User ID: " << user.id << ", Name: " << user.name << std::endl;
+
+        json userJson = user.toJson();
+
+        std::cout << "User JSON: \n"<< userJson.dump(4) << std::endl; 
+    }
+    
+    
+    return 0;
+}
